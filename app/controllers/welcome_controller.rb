698a89205b26005937c953732106a864deb684e1
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-12 14:16:23 -0500

    #1 creando un metodo prueba para la lecturad de archivos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,14 @@
 class WelcomeController < ApplicationController
+
+  def readExcel   
+    workbook = RubyXL::Parser.parse("/Users/makingdevs/Downloads/06_Colima.xlsx")
+    workbook['Valor'].each { |row|
+      row && row.cells.each { |cell|
+        val = cell && cell.value
+        puts val
+      }
+    }
+    render json: Date.new()
+  end
+
 end
