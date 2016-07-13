fcb1d3872727d8d8c6f4b0b178f2bc343f71a0cd
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-13 08:08:01 -0500

    #1 se ajusta metodo temporal para comprobar la lectura del excel falta recoger valores necesarios para persistencia

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,20 @@
 class WelcomeController < ApplicationController
 
   def readExcel   
+    header = [0,1,2,3,4]
+    resultRow = []
     workbook = RubyXL::Parser.parse("/Users/makingdevs/Downloads/06_Colima.xlsx")
-    workbook['Valor'].each { |row|
-      row && row.cells.each { |cell|
-        val = cell && cell.value
-        puts val
-      }
+    workbook['Valor'].each_with_index { |row, index|  
+      if row && !(header.include? index)
+        row.cells.each { |cell|
+          val = cell && cell.value
+          resultRow << val
+        }
+        puts resultRow.inspect()
+        puts "resultRow"
+        resultRow = []
+      end
     }
     render json: Date.new()
   end
-
 end
