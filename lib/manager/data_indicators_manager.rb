add3ffd77d3901addfbe09033d99c18350b7f9f4
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-14 12:26:41 -0500

    #3 la creacion de un simple data indicator

--- lib/manager/data_indicators_manager.rb
+++ lib/manager/data_indicators_manager.rb
@@ -4,7 +4,10 @@ class DataIndicatorsManager
 
   include Singleton
 
-  def create
+  def create(dataIndicator)
+    dataIndicator = DataIndicator.new(dataIndicator)
+    dataIndicator.save
+    dataIndicator
   end
 
 end
