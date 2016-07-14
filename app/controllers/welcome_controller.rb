ed5b33d6cdea58eab77828bb7a768bb8da72fa30
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-14 16:36:22 -0500

    #4 se agrega cosangas de jj para leer el tsv bien fresqui

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,20 @@
 require 'manager/migrate_data_manager'
+require 'daru'
 
 class WelcomeController < ApplicationController
 
-  def readExcel
+  def read_excel
     migrateDataManager = MigrateDataManager.instance
     Thread.new{
       migrateDataManager.migrate
     }
     render json: Date.new()
+  end
+
+  def data_sample
+    s = IO.read("/Users/makingdevs/Desktop/worldcupplayerinfo_20140701.tsv")
+    data_frame = Daru::DataFrame.from_csv("/Users/makingdevs/Desktop/worldcupplayerinfo_20140701.tsv", { :col_sep => "\t"})
+    p data_frame
+    render plain: s
   end
 end
