e160381ba58c03324b5883491f0d1c2e2a82a706
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-14 12:06:11 -0500

    #3 realizando la llamada al lector de excel para el proceso de la info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,11 @@
+require 'excel_reader'
+
 class WelcomeController < ApplicationController
 
-  def readExcel   
-    header = [0,1,2,3,4]
-    resultRow = []
-    workbook = RubyXL::Parser.parse("/Users/makingdevs/Downloads/06_Colima.xlsx")
-    workbook['Valor'].each_with_index { |row, index|  
-      if row && !(header.include? index)
-        row.cells.each { |cell|
-          val = cell && cell.value
-          resultRow << val
-        }
-        puts resultRow.inspect()
-        puts "resultRow"
-        resultRow = []
-      end
+  def readExcel
+    obj = ExcelReader.instance
+    Thread.new{
+      list = obj.read_simple_excel
     }
     render json: Date.new()
   end
