230d21ca5deda5cf34092bdb67ad6800bd1d6040
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-14 16:36:47 -0500

    #4 se agrega mapeo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
+
   resources :data_indicators
   get '/read', to: 'welcome#readExcel'
+  get '/data', to: 'welcome#data_sample'
   root 'welcome#index'
-  
+
 end
