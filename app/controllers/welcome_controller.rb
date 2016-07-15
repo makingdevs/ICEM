f021fbd48d044020886362ddfe39634cd1537253
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-15 11:29:34 -0500

    #4 con daru se manipula los datos para mostrar las personas de 25 años por pais

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,7 +14,10 @@ class WelcomeController < ApplicationController
   def data_sample
     s = IO.read("/Users/makingdevs/Desktop/worldcupplayerinfo_20140701.tsv")
     data_frame = Daru::DataFrame.from_csv("/Users/makingdevs/Desktop/worldcupplayerinfo_20140701.tsv", { :col_sep => "\t"})
-    p data_frame
+    los_de_25 = data_frame.where(data_frame["Age"].eq(25))
+    groups = los_de_25.group_by("Country")
+    s = "Country\tSize\n"
+    groups.groups.each { |country,indexes| s << "#{country[0]}\t#{indexes.size}\n" }
     render plain: s
   end
 end
