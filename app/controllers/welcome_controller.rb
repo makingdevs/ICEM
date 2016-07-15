2a55fe7920b96dc2026fc146a0aadd1bb1f94b71
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-15 17:32:24 -0500

    #7 se toma y se rendere csv

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 require 'manager/migrate_data_manager'
 require 'daru'
+require 'open-uri'
+require 'csv'
 
 class WelcomeController < ApplicationController
 
@@ -12,12 +14,13 @@ class WelcomeController < ApplicationController
   end
 
   def data_sample
-    s = IO.read("/Users/makingdevs/Desktop/worldcupplayerinfo_20140701.tsv")
-    data_frame = Daru::DataFrame.from_csv("/Users/makingdevs/Desktop/worldcupplayerinfo_20140701.tsv", { :col_sep => "\t"})
-    los_de_25 = data_frame.where(data_frame["Age"].eq(25))
-    groups = los_de_25.group_by("Country")
-    s = "Country\tSize\n"
-    groups.groups.each { |country,indexes| s << "#{country[0]}\t#{indexes.size}\n" }
-    render plain: s
+    data_frame = Daru::DataFrame.from_csv("Aguascalientes.csv")
+    #content = open('http://icem.com.s3.amazonaws.com/01_Aguascalientes')
+    #df = Daru::DataFrame.from_csv(houses)
+    #los_de_25 = data_frame.where(data_frame["Age"].eq(25))
+    #groups = los_de_25.group_by("Country")
+    #s = ""
+    #data_frame.groups.each { |country,indexes| s << "#{country[0]}\t#{indexes.size}\n" }
+    render html: data_frame
   end
 end
