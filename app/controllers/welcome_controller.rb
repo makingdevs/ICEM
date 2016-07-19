7d0bd3d9a9e6180a7fb2594aef3001299b1e726b
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-19 17:04:40 -0500

    #12 ajustando restantes del tsv

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,25 +5,30 @@ require 'open-uri'
 class WelcomeController < ApplicationController
 
   def data_sample
-    #content1 = open('http://icem.com.s3.amazonaws.com/Aguascalientes.csv')
-    #content2 = open('https://raw.githubusercontent.com/makingdevs/ICEM/master/Aguascalientes.csv')
     data_manager = DataProvider.instance
     data_frame_aguascalientes = data_manager.data_frame_aguascalientes
     data_frame_baja_california = data_manager.data_frame_baja_california
-    #filtrar Seguridad pública y justicia
-    #security = data_frame.where(data_frame["Tema_nivel_1"].eq('Seguridad pública y justicia'))
-    #filtrar delitos
+    data_frame_baja_california_sur = data_manager.data_frame_baja_california_sur
+    data_frame_campeche = data_manager.data_frame_campeche
+    data_frame_coahuila_de_zaragoza = data_manager.data_frame_coahuila_de_zaragoza
+
     security_aguascalientes = data_frame_aguascalientes.where(data_frame_aguascalientes["UnidadMedida"].eq('Delitos'))
     security_baja_california = data_frame_baja_california.where(data_frame_aguascalientes["UnidadMedida"].eq('Delitos'))
-    #filtrar estatal
-    #security = data_frame.where(data_frame["Desc_municipio"].eq('Estatal'))
-    #filtrar Delitos registrados 
+    security_baja_california_sur = data_frame_baja_california_sur.where(data_frame_baja_california_sur["UnidadMedida"].eq('Delitos'))
+    security_campeche = data_frame_campeche.where(data_frame_campeche["UnidadMedida"].eq('Delitos'))
+    security_coahuila_de_zaragoza = data_frame_coahuila_de_zaragoza.where(data_frame_coahuila_de_zaragoza["UnidadMedida"].eq('Delitos'))
+
     security_aguascalientes = security_aguascalientes.where(security_aguascalientes["Tema_nivel_2"].eq('Delitos registrados'))
     security_baja_california = security_baja_california.where(security_baja_california["Tema_nivel_2"].eq('Delitos registrados'))
-    s = "date\tAguascalientes\tBaja California\n"
+    security_baja_california_sur = security_baja_california_sur.where(security_baja_california_sur["Tema_nivel_2"].eq('Delitos registrados'))
+    security_campeche = security_campeche.where(security_campeche["Tema_nivel_2"].eq('Delitos registrados'))
+    security_coahuila_de_zaragoza = security_coahuila_de_zaragoza.where(security_coahuila_de_zaragoza["Tema_nivel_2"].eq('Delitos registrados'))
+
+    s = "date\tAguascalientes\tBaja California\tBaja California Sur\tCampeche\tCoahuila de Zaragoza\n"
     (2010..2015).each do |year|
-      s << "#{year}0101\t#{security_aguascalientes[year].sum || 0}\t#{security_baja_california[year].sum || 0}\n"
+      s << "#{year}0101\t#{security_aguascalientes[year].sum || 0}\t#{security_baja_california[year].sum || 0}\t#{security_baja_california_sur[year].sum || 0}\t#{security_campeche[year].sum || 0}\t#{security_coahuila_de_zaragoza[year].sum || 0}\n"
     end
+
     render plain:s
   end
 
