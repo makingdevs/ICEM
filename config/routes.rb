a55c4f7884c5505c9ca9dd6cff4b453caeaf6f17
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-19 16:33:11 -0500

    #12 eliminando un recurso y una ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :data_indicators
-  get '/read', to: 'welcome#readExcel'
   get '/data', to: 'welcome#data_sample'
   get '/security', to: 'welcome#security'
   root 'welcome#index'
