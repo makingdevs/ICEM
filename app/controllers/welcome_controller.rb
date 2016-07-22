9ca84d5bdd5f3e582047367cd1c9b7ab9bc2775f
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-22 13:16:05 -0500

    #17 ajustando controller y agregando enum

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 require 'manager/data_provider'
-require 'daru'
-require 'open-uri'
+require 'enums/states'
 
 class WelcomeController < ApplicationController
 
@@ -8,22 +7,23 @@ class WelcomeController < ApplicationController
     startRange = (params[:startRange] || 2010).to_i
     endRange = (params[:endRange] || 2015).to_i
     data_manager = DataProvider.instance
-    data_frames = data_manager.getDataFrames(params[:state] || "Ags,BC,BCS,Camp,Coah")
+    data_frames = data_manager.getDataFrames(params[:state])
     security_data_frames = {}
 
+    #TODO metodo que filtre cualquier data frame por algun parametro
     data_frames.each do |state, data_frame|
       security_data_frame = data_frame.where(data_frame["UnidadMedida"].eq('Delitos'))
       security_data_frames[state] = security_data_frame.where(security_data_frame["Tema_nivel_2"].eq('Delitos registrados')) 
     end
 
+    #TODO Esto seria un metodo que forme el encabezado
     s = "date"
-
     security_data_frames.each do |state, data_frame|
-      s << "\t#{state}"
+      s << "\t#{States.const_get(state)}"
     end
-
     s <<"\n"
 
+    #TODO Esto seria un metodo que forme el cuerpo del tsv
     (startRange..endRange).each do |year|
       s << "#{year}0101"
       data_frames.keys.each do |state|
@@ -31,10 +31,8 @@ class WelcomeController < ApplicationController
       end
       s << "\n"
     end
-    render plain:s
-  end
 
-  def security
+    render plain:s
   end
 
 end
