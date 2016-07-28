4baea62fa2c14ed70f955784ec0b84cefef5fa61
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-28 08:29:45 -0500

    #23 se agrega dos url para la grafica ejemplo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/data', to: 'welcome#data_sample'
   get '/security', to: 'welcome#security'
+  get '/example', to: 'welcome#example'
+  get '/renderDataExampe', to: 'welcome#renderDataExampe'
   root 'welcome#index'
 
 end
