67b97ca893ebd8db3575b71cad0ccd4e4b46af74
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-29 16:53:23 -0500

    #25 se eleimina las rutas que ya no se ocupan ya se borraron los metodos de controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   get '/data', to: 'welcome#data_sample'
-  get '/security', to: 'welcome#security'
-  get '/example', to: 'welcome#example'
-  get '/renderDataExampe', to: 'welcome#renderDataExampe'
   root 'welcome#index'
 
 end
