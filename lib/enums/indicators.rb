77a812e40f9549f17ce787e21112312e3cd792f5
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-03 16:48:47 -0500

    #30 se agrega enum de indicadores para la app

---
+++ lib/enums/indicators.rb
@@ -0,0 +1,15 @@
+module Indicators
+
+  VI = "Vivienda"
+  IN = "Ingresos"
+  EM = "Empleos"
+  CO = "Comunidad"
+  ED = "Educación"
+  EN = "Entorno"
+  CC = "Compromiso civil"
+  SA = "Salud"
+  SV = "Satisfacción de vida"
+  SE = "Seguridad"
+  ET = "Equilibrio trabajo-vida"
+
+end
