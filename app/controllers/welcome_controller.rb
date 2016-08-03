046cad94e22f6a6b78812ba360d840ef57a71676
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-03 16:47:44 -0500

    #30 en el metodo de index se agrega los indicadores y estados

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,14 @@
 require 'open-uri'
+require 'enums/states'
+require 'enums/indicators'
 
 class WelcomeController < ApplicationController
+
+  def index
+    @indicators = Indicators.constants.collect  do |indicator| Indicators.const_get(indicator) end
+    @states = States.constants.collect  do |state| States.const_get(state) end
+    render states: @states, indicators: @indicators
+  end
 
   def data_sample
     render plain: open('http://icem.com.s3.amazonaws.com/data_states.csv'){ |f| f.read }
