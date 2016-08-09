ec9faa0f62160486d57e03c38b49cb784dd32b6f
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-09 08:44:23 -0500

    #33 se agrega url para los indicadores

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/data', to: 'welcome#data_sample'
   get '/states', to: 'states#states'
   get '/about', to: 'about#index'
+  get '/indicators', to: 'indicators#indicators'
   root 'welcome#index'
 
 end
