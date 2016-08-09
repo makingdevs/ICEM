0254b87a89bfc1b22d1e138d9718e4ca48c9e828
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-09 11:16:38 -0500

    #42 agregando ruta para los datos de la grafica de indicadores

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/states', to: 'states#states'
   get '/about', to: 'about#index'
   get '/indicators', to: 'indicators#indicators'
+  get '/indicators/data', to: 'indicators#data'
   root 'welcome#index'
 
 end
