bd0a7b1e7a3b436ac240abbef84cdc5a318b6708
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-22 12:18:44 -0500

    #61 se ingresa modelo en duro en el controller con datos random para pintar un indice

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,7 +1,5 @@
 require 'enums/states'
 require 'enums/indicators'
-require 'csv'
-require 'json'
 
 class ExampleController < ApplicationController
 
@@ -12,6 +10,140 @@ class ExampleController < ApplicationController
   end
 
   def data_sample
+    render json: [
+                    {
+                      'hc-key': 'mx-3622',
+                      'value': 10
+                    },
+                    {
+                      'hc-key': 'mx-bc',
+                      'value': 1
+                    },
+                    {
+                      'hc-key': 'mx-bs',
+                      'value': 2
+                    },
+                    {
+                      'hc-key': 'mx-so',
+                      'value': 2
+                    },
+                    {
+                      'hc-key': 'mx-cl',
+                      'value': 1
+                    },
+                    {
+                      'hc-key': 'mx-na',
+                      'value': 10
+                    },
+                    {
+                      'hc-key': 'mx-cm',
+                      'value': 6
+                    },
+                    {
+                      'hc-key': 'mx-qr',
+                      'value': 7
+                    },
+                    {
+                      'hc-key': 'mx-mx',
+                      'value': 8
+                    },
+                    {
+                      'hc-key': 'mx-mo',
+                      'value': 9
+                    },
+                    {
+                      'hc-key': 'mx-df',
+                      'value': 10
+                    },
+                    {
+                      'hc-key': 'mx-qt',
+                      'value': 1
+                    },
+                    {
+                      'hc-key': 'mx-tb',
+                      'value': 2
+                    },
+                    {
+                      'hc-key': 'mx-cs',
+                      'value': 3
+                    },
+                    {
+                      'hc-key': 'mx-nl',
+                      'value': 4
+                    },
+                    {
+                      'hc-key': 'mx-si',
+                      'value': 6
+                    },
+                    {
+                      'hc-key': 'mx-ch',
+                      'value': 6
+                    },
+                    {
+                      'hc-key': 'mx-ve',
+                      'value': 7
+                    },
+                    {
+                      'hc-key': 'mx-za',
+                      'value': 8
+                    },
+                    {
+                      'hc-key': 'mx-ag',
+                      'value': 9
+                    },
+                    {
+                      'hc-key': 'mx-ja',
+                      'value': 2
+                    },
+                    {
+                      'hc-key': 'mx-mi',
+                      'value': 1
+                    },
+                    {
+                      'hc-key': 'mx-oa',
+                      'value': 2
+                    },
+                    {
+                      'hc-key': 'mx-pu',
+                      'value': 3
+                    },
+                    {
+                      'hc-key': 'mx-gr',
+                      'value': 24
+                    },
+                    {
+                      'hc-key': 'mx-tl',
+                      'value': 25
+                    },
+                    {
+                      'hc-key': 'mx-tm',
+                      'value': 6
+                    },
+                    {
+                      'hc-key': 'mx-co',
+                      'value': 7
+                    },
+                    {
+                      'hc-key': 'mx-yu',
+                      'value': 8
+                    },
+                    {
+                      'hc-key': 'mx-dg',
+                      'value': 2
+                    },
+                    {
+                      'hc-key': 'mx-gj',
+                      'value': 3
+                    },
+                    {
+                      'hc-key': 'mx-sl',
+                      'value': 3
+                    },
+                    {
+                      'hc-key': 'mx-hg',
+                      'value': 2
+                    }
+                  ]
   end
 
 end
