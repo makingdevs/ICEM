496f65eace378d09cf3fb66a1ea8244c15f84d6f
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-23 12:22:48 -0500

    #66 se agrega url para pedir los datos del mapa de indicadores de estados

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   get '/data', to: 'welcome#data_sample'
   get '/states', to: 'states#states'
+  get '/states/data', to: 'states#data'
   get '/about', to: 'about#index'
   get '/indicators', to: 'indicators#indicators'
   get '/indicators/data', to: 'indicators#data'
