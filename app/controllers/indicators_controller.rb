a60a68d07f179b9fdad1308f9f8dd4559fc10a11
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-23 09:34:59 -0500

    #64 ajustando como se obtienen los datos de los indicadores

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -6,7 +6,7 @@ class IndicatorsController < ApplicationController
 
   def indicators
     p params[:name]
-    @indicators = Indicators.constants.collect  do |indicator| Indicators.const_get(indicator) end
+    @indicators = Indicators.constants.collect  do |indicator| Indicators.const_get(indicator).split(",")[0] end
     @states = States.constants.collect  do |state| States.const_get(state) end
     render states: @states, Indicators: @indicators
   end
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -5,7 +5,7 @@ class StatesController < ApplicationController
 
   def states
     p params[:name]
-    @indicators = Indicators.constants.collect  do |indicator| Indicators.const_get(indicator) end
+    @indicators = Indicators.constants.collect  do |indicator| Indicators.const_get(indicator).split(",")[0] end
     @states = States.constants.collect  do |state| States.const_get(state) end
     render states: @states, Indicators: @indicators
   end
