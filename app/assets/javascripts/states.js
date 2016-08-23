3b27be359d447b6a3edbd46fd738451633378d6a
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-23 12:20:51 -0500

    #66 se ajusta el control del año en el js de estados

--- app/assets/javascripts/states.js
+++ app/assets/javascripts/states.js
@@ -10,9 +10,9 @@ width = 960 - margin.right,
 height = 500 - margin.top - margin.bottom;
 
 // Various scales. These domains make assumptions of data, naturally.
-var xScale = d3.scale.log().domain([300, 1e5]).range([0, width]),
+var xScale = d3.scale.log().domain([100, 5e4]).range([0, width]),
 yScale = d3.scale.linear().domain([10, 85]).range([height, 0]),
-radiusScale = d3.scale.sqrt().domain([0, 5e8]).range([0, 40]),
+radiusScale = d3.scale.sqrt().domain([0, 5e7]).range([0, 40]),
 colorScale = d3.scale.category10();
 
 // The x & y axes.
@@ -60,19 +60,19 @@ var label = svg.append("text")
 .attr("text-anchor", "end")
 .attr("y", height - 24)
 .attr("x", width)
-.text(1800);
+.text(1990);
 
 // Load the data.
-d3.json("https://bost.ocks.org/mike/nations/nations.json", function(nations) {
+d3.json("/states/data", function(nations) {
 
 // A bisector since many nation's data is sparsely-defined.
 var bisect = d3.bisector(function(d) { return d[0]; });
 
-// Add a dot per nation. Initialize the data at 1800, and set the colors.
+// Add a dot per nation. Initialize the data at 1990, and set the colors.
 var dot = svg.append("g")
 .attr("class", "dots")
 .selectAll(".dot")
-.data(interpolateData(1800))
+.data(interpolateData(1990))
 .enter().append("circle")
 .attr("class", "dot")
 .style("fill", function(d) { return colorScale(color(d)); })
@@ -116,7 +116,7 @@ function order(a, b) {
 // After the transition finishes, you can mouseover to change the year.
 function enableInteraction() {
   var yearScale = d3.scale.linear()
-  .domain([1800, 2009])
+  .domain([1990, 2009])
   .range([box.x + 10, box.x + box.width - 10])
   .clamp(true);
 
@@ -145,7 +145,7 @@ function mousemove() {
 // Tweens the entire chart by first tweening the year, and then the data.
 // For the interpolated data, the dots and label are redrawn.
 function tweenYear() {
-  var year = d3.interpolateNumber(1800, 2009);
+  var year = d3.interpolateNumber(1990, 2009);
   return function(t) { displayYear(year(t)); };
 }
 
