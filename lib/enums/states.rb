e50f153c4e0588972d0bffd0a716041558de8454
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-25 10:45:32 -0500

    #69 refactor del enum de estados esto para obtener los valores de mejor manera

--- lib/enums/states.rb
+++ lib/enums/states.rb
@@ -1,34 +1,44 @@
 module States
-  AGS = "Aguascalientes"
-  BC = "Baja California"
-  BCS = "Baja California Sur"
-  CAMP = "Campeche"
-  COAH = "Coahuila"
-  COL = "Colima"
-  CHIS = "Chiapas"
-  CHIH = "Chihuahua"
-  DF = "Ciudad de México"
-  DGO = "Durango"
-  GTO = "Guanajuato"
-  GRO = "Guerrero"
-  HGO = "Hidalgo"
-  JAL = "Jalisco"
-  MEX = "Estado de México"
-  MICH = "Michoacán"
-  MOR = "Morelos"
-  NAY = "Nayarit"
-  NL = "Nuevo León"
-  OAX = "Oaxaca"
-  PUE = "Puebla"
-  QRO = "Querétaro"
-  QR = "Quintana Roo"
-  SLP = "San Luis Potosí"
-  SIN = "Sinaloa"
-  SON = "Sonora"
-  TAB = "Tabasco"
-  TAMS = "Tamaulipas"
-  TLAX = "Tlaxcala"
-  VER = "Veracruz"
-  YUC = "Yucatán"
-  ZAC = "Zacatecas"
+
+  @@values = 
+    {
+      :AGS => "Aguascalientes",
+      :BC => "Baja California",
+      :BCS => "Baja California Sur",
+      :CAMP => "Campeche",
+      :COAH => "Coahuila",
+      :COL => "Colima",
+      :CHIS => "Chiapas",
+      :CHIH => "Chihuahua",
+      :DF => "Ciudad de México",
+      :DGO => "Durango",
+      :GTO => "Guanajuato",
+      :GRO => "Guerrero",
+      :HGO => "Hidalgo",
+      :JAL => "Jalisco",
+      :MEX => "Estado de México",
+      :MICH => "Michoacán",
+      :MOR => "Morelos",
+      :NAY => "Nayarit",
+      :NL => "Nuevo León",
+      :OAX => "Oaxaca",
+      :PUE => "Puebla",
+      :QRO => "Querétaro",
+      :QR => "Quintana Roo",
+      :SLP => "San Luis Potosí",
+      :SIN => "Sinaloa",
+      :SON => "Sonora",
+      :TAB => "Tabasco",
+      :TAMS => "Tamaulipas",
+      :TLAX => "Tlaxcala",
+      :VER => "Veracruz",
+      :YUC => "Yucatán",
+      :ZAC => "Zacatecas"
+    }
+
+  def self.values
+    @@values
+  end
+
 end
+
