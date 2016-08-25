1451bf2fe05289e7b7ff230e2be8c65853588ea1
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-08-25 10:44:35 -0500

    #69 se ajusta la lista de estados y se ajusta el render del view del estados segun el parametro

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -4,10 +4,9 @@ require 'enums/indicators'
 class StatesController < ApplicationController
 
   def states
-    p params[:name]
     @indicators = Indicators.constants.collect  do |indicator| Indicators.const_get(indicator).split(",")[0] end
-    @states = States.constants.collect  do |state| States.const_get(state) end
-    render states: @states, Indicators: @indicators
+    @states = States.values
+    render "states/#{params[:name].downcase}",  states: @states, Indicators: @indicators
   end
 
   def data
