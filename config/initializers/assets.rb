21c9336046bfb3735df017f4e7bd265139918bac
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-09-08 15:44:48 -0500

    #76 agregando el compile de los font que se encuntran en los assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,8 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("vendor","assets","third-party")
 
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 %w( welcome states about indicators).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
 end
